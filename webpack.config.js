9def3659cb577e2150ec73a2cce23b5eab1a3f15
mireiaGomariz <mireiapgomariz@gmail.com>
2020-05-22 11:11:52 +0200

    add data loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,44 @@
 const path = require('path');
 
- module.exports = {
-   entry: './src/index.js',
-   output: {
-     filename: 'bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
-   module: {
-     rules: [
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
        {
-         test: /\.css$/,
+         test: /\.(csv|tsv)$/,
          use: [
-           'style-loader',
-           'css-loader'
+           'csv-loader',
          ],
        },
        {
-         test: /\.(png|svg|jpg|gif)$/,
+         test: /\.xml$/,
          use: [
-           'file-loader',
+           'xml-loader',
          ],
        },
-     ],
-   },
- };
+    ],
+  },
+};
