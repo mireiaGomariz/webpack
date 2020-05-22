bc9e07b9a6ecbaa28cb2d5a7f6fe4a28f6153b49
mireiaGomariz <mireiapgomariz@gmail.com>
2020-05-22 11:43:19 +0200

    development

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,20 @@ const path = require('path');
  const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: './src/index.js',
     print: './src/print.js',
   },
+  devtool: 'inline-source-map',
+     devServer: {
+     contentBase: './dist',
+   },
+  devServer: {
+     contentBase: './dist',
+   },
    plugins: [
-     new CleanWebpackPlugin(),
+     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
      new HtmlWebpackPlugin({
        title: 'Output Management',
      }),
@@ -16,5 +24,6 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
 };
