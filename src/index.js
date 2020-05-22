8ba08aee01f5285104a96a2fabe8534e51ca8ffd
mireiaGomariz <mireiapgomariz@gmail.com>
2020-05-22 11:22:55 +0200

    HtmlWebpackPluggin

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+import printMe from './print.js';
+
+
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-     // Add the image to our existing div.
-   const myIcon = new Image();
-   myIcon.src = Icon;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-   element.appendChild(myIcon);
-   console.log(Data);
+  element.appendChild(btn);
+
+
 
   return element;
 }
